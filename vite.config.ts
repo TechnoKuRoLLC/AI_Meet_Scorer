ccdc1f211074b831d2ac89079b774dd44903d1f7
Nikita Kamaev <hiyori.amatsuki@gmail.com>
2025-05-31 17:26:11 +0900

    add tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
+import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 // biome-ignore lint/style/noDefaultExport: <explanation>
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
 })
